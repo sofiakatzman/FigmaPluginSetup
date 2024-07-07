601496fec820402a849eb847f8c65c4e2e353768
Sofia Katzman <katzmansof@gmail.com>
2024-07-07 15:40:14 -0400

    add text replacement logic

--- code.ts
+++ code.ts
@@ -2,21 +2,62 @@ figma.showUI(__html__);
 figma.ui.resize(500, 500);
 
 figma.loadAllPagesAsync().then(() => {
-  figma.ui.onmessage = pluginMessage => {
-    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
-    const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
-    const defaultDark = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name =="Image=none, Dark mode=true") as ComponentNode;
-    
-    console.log(pluginMessage.name);
-    console.log(pluginMessage.username);
-    console.log(pluginMessage.description);
-    console.log(pluginMessage.darkModeState);
-    console.log(pluginMessage.imageVariant);
+  figma.ui.onmessage = async pluginMessage => {
+    console.log("Received message:", pluginMessage);
 
-    if (pluginMessage.darkModeState === true) {
-      defaultDark.createInstance();
+    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
+    let selectedVariant = null;
+
+    if (postComponentSet) {
+      console.log("Found postComponentSet:", postComponentSet);
+
+      if (pluginMessage.darkModeState === true) {
+        switch (pluginMessage.imageVariant) {
+          case "2":
+            selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=true") as ComponentNode;
+            break;
+          case "3":
+            selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=true") as ComponentNode;
+            break;
+          default:
+            selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+            break;
+        }
+      } else {
+        switch (pluginMessage.imageVariant) {
+          case "2":
+            selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=false") as ComponentNode;
+            break;
+          case "3":
+            selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=false") as ComponentNode;
+            break;
+          default:
+            selectedVariant = postComponentSet.defaultVariant as ComponentNode;
+            break;
+        }
+      }
+
+      if (selectedVariant) {
+        console.log("Found selectedVariant:", selectedVariant);
+        // create instance based on selected variant
+        const newPost = selectedVariant.createInstance();
+        // find text fields in new instance
+        const templateName= newPost.findOne(node => node.name == "displayName" && node.type == "TEXT") as TextNode;
+        const templateUserName= newPost.findOne(node => node.name == "@username" && node.type == "TEXT") as TextNode;
+        const templateDescription= newPost.findOne(node => node.name == "description" && node.type == "TEXT") as TextNode;
+        
+        // Load fonts
+        await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
+        
+        // Replace text of new instances
+        templateName.characters = pluginMessage.name;
+        templateUserName.characters = pluginMessage.username;
+        templateDescription.characters = pluginMessage.description;
+      } else {
+        console.error("No matching component found for the given criteria.");
+      }
     } else {
-      defaultVariant.createInstance();
+      console.error("Component set with the name 'post' not found");
     }
 
     figma.closePlugin();
