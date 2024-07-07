1232ca5acaa413b06cab9bcc6dc210fc5f5b568c
Sofia Katzman <katzmansof@gmail.com>
2024-07-07 15:03:37 -0400

    added darkmode component creation logic

--- code.ts
+++ code.ts
@@ -1,10 +1,27 @@
 figma.showUI(__html__);
-figma.ui.resize(500,500);
+figma.ui.resize(500, 500);
 
-figma.ui.onmessage = pluginMessage=>{
-  console.log(pluginMessage.name);
-  console.log(pluginMessage.username);
-  console.log(pluginMessage.description);
-  console.log(pluginMessage.darkModeState);
-  console.log(pluginMessage.imageVariant);
-}
+figma.loadAllPagesAsync().then(() => {
+  figma.ui.onmessage = pluginMessage => {
+    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
+    const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+    const defaultDark = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name =="Image=none, Dark mode=true") as ComponentNode;
+    
+    console.log(pluginMessage.name);
+    console.log(pluginMessage.username);
+    console.log(pluginMessage.description);
+    console.log(pluginMessage.darkModeState);
+    console.log(pluginMessage.imageVariant);
+
+    if (pluginMessage.darkModeState === true) {
+      defaultDark.createInstance();
+    } else {
+      defaultVariant.createInstance();
+    }
+
+    figma.closePlugin();
+  };
+}).catch(error => {
+  console.error("Error loading pages:", error);
+  figma.closePlugin();
+});
